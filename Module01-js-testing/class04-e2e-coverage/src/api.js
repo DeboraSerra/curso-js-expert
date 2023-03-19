2ba5acc86143e7d376739cd3341a5d9f3660b51c
Debora Serra <debora.r.serra@gmail.com>
2023-03-19 11:28:11 -0300

    feat: add api

---
+++ Module01-js-testing/class04-e2e-coverage/src/api.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const { once } = require('events');
+
+const DEFAUL_USER = {
+  username: 'DebsSerra',
+  password: '123456',
+}
+
+const routes = {
+  '/login:post': async (req, res) => {
+    const { username, password } = JSON.parse(await once(req, "data"));
+    if (username !== DEFAUL_USER.username || password !== DEFAUL_USER.password) {
+      res.writeHead(401);
+      return res.end('Invalid username or password')
+    }
+    return res.end('ok')
+  },
+  default: (req, res) => {
+    res.writeHead(404);
+    res.end('Route not found');
+  },
+}
+
+const handler = (req, res) => {
+  const { url, method } = req;
+  const path = `${url.toLowerCase()}:${method.toLowerCase()}`;
+  const route = routes[path] || routes.default;
+  return route(req, res);
+}
+
+const app = http.createServer(handler).listen(3000, () => console.log(3000))
+
+module.exports = app;
